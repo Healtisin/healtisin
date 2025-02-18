6d21fbf3fb691b3f3a0bb660aaaa61284dca4547
Luthfi Abdillah <healtisin@gmail.com>
2025-02-19 05:13:22 +0700

    Button Delete sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -50,7 +50,7 @@
                         </div>
                     </button>
                     <button onclick="showDeleteChatModal({{ $history->id }})" 
-                            class="absolute right-2 top-1/2 -translate-y-1/2 p-2 rounded-full hover:bg-red-50 text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
+                            class="absolute right-2 top-1/2 -translate-y-1/2 p-2 rounded-full hover:bg-red-50 text-red-500 opacity-0 group-hover:opacity-100 transition-opacity sidebar-text">
                         <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                         </svg>
